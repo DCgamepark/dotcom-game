c359062a97a29a93f58cf3f167e4a55e5779a475
DCgamepark <hh.890metro@gmail.com>
2026-09-18 13:49:26 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://qefopqgfpetbgiqigyax.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_vc-Tx6c-mken5RpOyjkvQw_PcvWXrZR";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
